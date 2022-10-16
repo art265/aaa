cdf1a4c3be8c316a20c90738f1aa83e02db13905
Jareer Abdullah <jforeverything2007@gmail.com>
2022-10-16 16:30:18 +0200

    Bye Bye CSS

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # panel
- 
+
+Continuing the legacy of [Discord-bot-panel](https://github.com/jareer12/Discord-Bot-Dashboard)
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
-import App from "./App.vue";
 import router from "./router";
-
-import "./assets/main.css";
+import App from "./App.vue";
 
 const app = createApp(App);
 
