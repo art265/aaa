006df7f0d88c89756d24fad7d438781f34401dcb
Jareer Abdullah <jforeverything2007@gmail.com>
2022-10-17 20:22:01 +0200

    Update

---
+++ client/src/components/ToggleButton.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: {
+    On: {
+      type: Boolean,
+      default: false,
+    },
+    ClassName: {
+      type: String,
+      default: "",
+    },
+    onClicked: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      localStorage: localStorage,
+    };
+  },
+};
+</script>
+
+<template>
+  <div
+    class="bg-steel-300 grid grid-cols-2 rounded-lg cursor-pointer"
+    v-on:click="onClicked"
+  >
+    <div v-if="On === false"></div>
+    <div
+      :class="`${ClassName} rounded-lg text-center items-center flex justify-center uppercase font-semibold bg-gradient-to-r from-${localStorage.theme}-300 to-${localStorage.theme}-500`"
+    >
+      {{ On === false ? "On" : "Off" }}
+    </div>
+  </div>
+</template>
--- client/src/components/settings/AccountBox.vue
+++ client/src/components/settings/AccountBox.vue
@@ -1,29 +1,29 @@
 <script>
+import ManageAccount from "@/components/screen/ManageAccount.vue";
+
 export default {
+  components: { ManageAccount },
   data() {
     return {
+      VisibleDisplay: false,
       localStorage: localStorage,
     };
   },
 };
 </script>
+
 <template>
   <main>
     <div class="gap-3 mt-3 grid">
-      <div
-        :class="`w-full
-              rounded-lg
-              p-3
-              bg-gradient-to-b
-              from-steel-300
-              to-steel-400
-`"
-      >
+      <div :class="`w-full
+                    rounded-lg
+                    p-3
+                    bg-gradient-to-b
+                    from-steel-300
+                    to-steel-400
+      `">
         <div class="bg-steel-200">
-          <img
-            src="https://avatars.githubusercontent.com/u/57962439?v=4"
-            class="rounded-lg object-cover w-full"
-          />
+          <img src="https://avatars.githubusercontent.com/u/57962439?v=4" class="rounded-lg object-cover w-full" />
         </div>
         <div class="border border-steel-200 rounded-full mt-3"></div>
         <div class="mt-3">
@@ -32,11 +32,19 @@ export default {
             <span> ${Username} </span>
           </h3>
         </div>
-        <button
-          :class="`mt-3 rounded-lg float-right w-full py-3 bg-gradient-to-r from-${localStorage.theme}-300 to-${localStorage.theme}-500`"
-        >
+        <button v-on:click="
+          () => {
+            VisibleDisplay = true;
+          }
+        "
+          :class="`mt-3 rounded-lg float-right w-full py-3 bg-gradient-to-r from-${localStorage.theme}-300 to-${localStorage.theme}-500`">
           Manage
         </button>
+        <ManageAccount :Visible="VisibleDisplay" :onCrossed="
+          () => {
+            VisibleDisplay = false;
+          }
+        "></ManageAccount>
       </div>
     </div>
   </main>
