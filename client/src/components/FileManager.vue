44febadc11772f763ffea9cbf018ef0222b3374e
Jareer Abdullah <jforeverything2007@gmail.com>
2022-10-19 12:59:42 +0200

    Update

---
+++ client/src/components/FileManager.vue
@@ -0,0 +1,70 @@
+<script>
+export default {
+  props: {
+    directories: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  methods: {
+    ColorToClass() {
+      this.DarkColor = !this.DarkColor;
+      return this.DarkColor == true ? "bg-steel-300" : "bg-steel-200";
+    },
+  },
+
+  data() {
+    return {
+      DarkColor: true,
+    };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h3>File Manager</h3>
+    <table class="table-auto text-start w-full bg-steel-300 rounded-lg p-3">
+      <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-300">
+          <thead
+            class="text-xs text-white uppercase border-b border-steel-400 bg-steel-300 dark:bg-gray-700 dark:text-gray-400"
+          >
+            <tr>
+              <th scope="col" class="py-3 px-6">Name</th>
+              <th scope="col" class="py-3 px-6">Type</th>
+              <th scope="col" class="py-3 px-6">Size</th>
+              <th scope="col" class="py-3 px-6">Price</th>
+              <th scope="col" class="py-3 px-6">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              :class="`${ColorToClass()} border-b border-steel-400`"
+              v-for="dir in directories"
+              :key="dir"
+            >
+              <th
+                scope="row"
+                class="py-4 px-6 font-medium whitespace-nowrap dark:text-white"
+              >
+                {{ dir.name }}
+              </th>
+              <td class="py-4 px-6">{{ dir.isDir ? "Folder" : "File" }}</td>
+              <td class="py-4 px-6">Laptop</td>
+              <td class="py-4 px-6">$2999</td>
+              <td class="py-4 px-6">
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                  >Edit</a
+                >
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </table>
+  </main>
+</template>
--- client/src/components/Instance.vue
+++ client/src/components/Instance.vue
@@ -1,8 +1,16 @@
 <script>
+import ToggleButton from "../components/ToggleButton.vue";
+import FileManager from "../components/FileManager.vue";
+
 export default {
-  components: {},
+  components: { ToggleButton, FileManager },
   data() {
     return {
+      AutoRestart: false,
+      directories: [
+        { name: "src", isDir: true },
+        { name: "index.js", isDir: false },
+      ],
       localStorage: localStorage,
     };
   },
@@ -10,5 +18,46 @@ export default {
 </script>
 
 <template>
-  <main class="py-5">d</main>
+  <main class="p-5 mt-5 rounded-lg bg-steel-400 grid grid-cols-2 gap-5">
+    <section :class="`grid-cols-5 grid`">
+      <label for="" :class="`col-span-5`">Target script</label>
+      <div :class="`col-span-4`">
+        <input
+          type="text"
+          placeholder="index.js"
+          class="p-3 bg-steel-300 rounded-lg w-full"
+        />
+      </div>
+      <div :class="`ml-3`">
+        <ToggleButton
+          :class="`h-10`"
+          :On="AutoRestart"
+          :onClicked="
+            () => {
+              AutoRestart = !AutoRestart;
+            }
+          "
+        />
+      </div>
+    </section>
+
+    <section :class="`grid-cols-5 grid`">
+      <label for="" :class="`col-span-5`">Target script</label>
+      <div :class="`col-span-4`">
+        <input
+          type="text"
+          placeholder="index.js"
+          class="p-3 bg-steel-300 rounded-lg w-full"
+        />
+      </div>
+      <div :class="`ml-3 flex`">
+        <select class="px-3 bg-steel-300 rounded-lg w-full">
+          <option value="">Node.js</option>
+          <option value="">Python</option>
+        </select>
+      </div>
+    </section>
+
+    <FileManager class="col-span-2" :directories="directories" />
+  </main>
 </template>
